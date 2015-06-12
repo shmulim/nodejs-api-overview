4d625301397c165dc8063bb1a6d294ebc41564e4
@shmulim <shmuel.markel@gmail.com>
2015-06-12 01:53:03 -0700

    Create listen_EADDRINUSE.js

---
+++ common-errors/listen_EADDRINUSE.js
@@ -0,0 +1,5 @@
+// example
+Error: listen EADDRINUSE 127.0.0.1:8080
+
+// This means the address you're trying to bind the server to is already in use.
+// Either close the open connection or assign your server to a different host/port.
