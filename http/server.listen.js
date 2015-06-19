5344c744e42f148392b9daf3ad96aeb91e8ae03d
@shmulim <shmuel.markel@gmail.com>
2015-06-19 08:35:00 -0700

    Update server.listen.js

--- http/server.listen.js
+++ http/server.listen.js
@@ -4,3 +4,8 @@ var server = http.createServer(function(request, response){
 });
 
 server.listen(8080, 'localhost');
+
+// chaining listener to server directly to server object
+var http = require('http');
+var server = http.createServer().listen(8080, 'localhost');
+// implement server event handlers here.
